e921359e4e83c7ba9a99770d937ec700d8ac4549
Gunter Mueller <gunter.mueller@gmail.com>
2020-07-11 12:28:31 -0400

    Another attempt to build this.

---
+++ Ingres_FOSSLC/server/build/files/iixa.h
@@ -0,0 +1,21 @@
+/*
+**	Copyright (c) 2007 Ingres Corporation
+*/
+
+#ifndef IIXA_H_INCLUDED
+#define IIXA_H_INCLUDED
+/*----------------  iixa.h  ---------------------------------------------*/
+
+
+/*
+** The exported variable declaration for the Ingres' xa_switch structure.
+**
+*/
+
+typedef struct  xa_switch_t  IIXA_SWITCH;
+
+extern  IIXA_SWITCH  iixa_switch;
+
+
+/*---------------  end of iixa.h  ----------------------------------------*/
+#endif /* IIXA_H_INCLUDED */
